f9c9b320bb2e4ad7745e4e6288aaf738d4c334d4
[XiuYu.Ge] <861719405@qq.com>
2018-09-04 13:27:31 +0800

    add user and pay model

---
+++ cloud-cli-user/cloud-cli-user-service/src/main/java/cn/zzcode/user/App.java
@@ -0,0 +1,13 @@
+package cn.zzcode.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
